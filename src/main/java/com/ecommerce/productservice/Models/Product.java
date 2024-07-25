516dc42a8c71cd5cd4fc8182d79e214afdee0f11
aswajbs <aswajbs@gmail.com>
2024-07-25 14:39:10 +0530

    Ecommerce Database connected using MappedSuperClass Inheritance type

--- src/main/java/com/ecommerce/productservice/Models/Product.java
+++ src/main/java/com/ecommerce/productservice/Models/Product.java
@@ -1,13 +1,17 @@
 package com.ecommerce.productservice.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
     private String title;
     private double price;
+    @ManyToOne
     private Category category;
 
 
